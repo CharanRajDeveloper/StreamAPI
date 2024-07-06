f7fcced4b31039130f498ee4cb198bd9fd74b876
charanraj <charanrajjb007@gmail.com>
2024-07-07 00:40:30 +0530

    Stream API

---
+++ src/com/streams/Student.java
@@ -0,0 +1,105 @@
+package com.streams;
+
+public class Student {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String gender;
+	private String departmantName;
+	private int joinedYear;
+	private String city;
+	private int rank;
+
+	public Student(int id, String firstName, String lastName, int age, String gender, String departmantName,
+			int joinedYear, String city, int rank) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.gender = gender;
+		this.departmantName = departmantName;
+		this.joinedYear = joinedYear;
+		this.city = city;
+		this.rank = rank;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDepartmantName() {
+		return departmantName;
+	}
+
+	public void setDepartmantName(String departmantName) {
+		this.departmantName = departmantName;
+	}
+
+	public int getJoinedYear() {
+		return joinedYear;
+	}
+
+	public void setJoinedYear(int joinedYear) {
+		this.joinedYear = joinedYear;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public String toString() {
+		return "ID:" + this.id + " lastName:" + this.lastName + " firstName" + this.firstName + "Age:" + this.age
+				+ " Gender:" + this.gender + " DepartmentName:" + this.departmantName + " Joined year:"
+				+ this.joinedYear + " City:" + this.city + " Rank:" + this.rank;
+	}
+}
